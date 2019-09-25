657312990c6807aa322e62f46450dc72e79e5ff7
EthanJF <ethanjosephfournier@gmail.com>
2019-09-25 10:14:21 -0400

    Added more test cases

--- tools/console.rb
+++ tools/console.rb
@@ -18,6 +18,10 @@ magazine3 = Magazine.new("Rolling Stone", "music")
 article1 = Article.new(author1, magazine1, "My Great Computer", "My computer is so great. It helps me write a lot.")
 article2 = Article.new(author2, magazine2, "Art is Cool", "I really like art. It helps me write my books. Aw yeah, I love art.")
 article3 = Article.new(author3, magazine3, "Music is Cool", "I really love music. It gets me in the zone when I'm writing my books. Yeah, music is awesome.")
+article4 = Article.new(author2, magazine1, "My Favorite Word Proccessor", "I really love Microsoft Word. It helps me get all of my writing done. I couldn't recommend a better word processor.")
+article5 = Article.new(author1, magazine3, "My Favorite Bands", "Some of my favorite bands are The Cure, Bauhaus, Sisters of Mercy, and Siouxsie and the Banshees.")
+article6 = Article.new(author2, magazine3, "I Like Music", "Music is great. I listed to music when I write. It helps me concentrate."
+)
 
 
 
