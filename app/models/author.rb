fe4cb19bde67fa573d91fc4e48bdbf2fd1c30a66
TheLazyLearner <eduardpola18@hotmail.com>
2019-09-25 10:37:50 -0400

    done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,31 @@
 class Author
-  attr_accessor :name
+  attr_accessor :title, :content, :magazine, :name
 
+  @@all = []
 
-  def initialize(name)
+  def initialize(title, content, magazine, name)
+    @title = title
+    @content = content
+    @magazine = magazine
     @name = name
-  
+    @@all << self
+  end
+
+  def articles
+    Article.all.select do |article|
+      article.magazine == self
+    end
+  end
+
+  def magazines
+    article.map do |article|
+      article.author
+    end
+  end
+
+
+  def self.all
+    @@all
   end
 
 
--- tools/console.rb
+++ tools/console.rb
@@ -8,7 +8,15 @@ end
 ###  WRITE YOUR TEST CODE HERE ###
 
 
+author1 = Author.new("foodie", "cooking", "food network", "Rick")
+author2 = Author.new("foodaholic", "food addiction", "food network", "James")
 
+magazine1 = Magazine.new("food", "Food addict")
+magazine2 = Magazine.new("drink", "alcoholic")
+
+
+article1 = Article.new(author1, magazine1, "Food Wars", "food")
+article2 = Article.new(author2, magazine2, "Drink Wars", "drink")
 
 
 
